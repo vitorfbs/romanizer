cd82ef6207ffb1c240a7b8e86d6c6d033a88b1c8
Vitor Ferreira Bem Silva <vitorfbs.ti@gmail.com>
2019-07-30 16:40:39 -0300

    TODO validation with different messages

--- api/controller/helpers/ValidationHelper.js
+++ api/controller/helpers/ValidationHelper.js
@@ -2,7 +2,8 @@
 // https://www.w3schools.com/jsref/jsref_match.asp
 
 function checkRequestValueIntegrity(value) {
-    if (value.length >5 || (parseInt(value[0]) == 0 && value.length > 1 )) {
+    //TODO make different outcoems for each failure in the user input
+    if (value.length > 5 || (parseInt(value[0]) == 0 && value.length > 1 )) {
         return null;
     }
     
