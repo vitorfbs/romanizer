594487ab4efdd6b9fedb94f53e7e2b0eebab6958
Vitor Ferreira Bem Silva <vitorfbs.ti@gmail.com>
2019-07-30 17:13:15 -0300

    Update of router position and reference comments

--- api/app.js
+++ api/app.js
@@ -1,8 +1,12 @@
+// References:
+// https://expressjs.com/en/starter/hello-world.html
+// https://jilles.me/getting-the-express-app-js/
+
 const express = require('express');
+const app = express();
 
 // Routes
-const indexRouter = require('./routes/router');
-const app = express();
+const indexRouter = require('./router');
 
 // Set all routes
 app.use('/', indexRouter);
