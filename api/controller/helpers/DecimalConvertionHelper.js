9b67f94f366b3ea8bb9e51993f459ed48003bdb2
Vitor Ferreira Bem Silva <vitorfbs.ti@gmail.com>
2019-07-30 17:44:36 -0300

    New helper for the decimal converters, created for clarity's sake

---
+++ api/controller/helpers/DecimalConvertionHelper.js
@@ -0,0 +1,48 @@
+const dictionary = require('../../dictionaries/pt-br/NumberDictionary');
+
+function unitsConverter(value){
+  if(value.length == 1){
+    return dictionary.units[parseInt(value)];
+  } else{
+    return dictionary.units[parseInt(value[value.length-1])];
+  }
+}
+
+function tensConverter(value){
+  if(parseInt(value[value.length-2]) == 1){
+    return dictionary.outliers[parseInt(value[value.length-1])];
+  } else {
+    return dictionary.tens[parseInt(value[value.length-2])];
+  }
+}
+
+function hundredsConverter(value){
+  if(parseInt(value[value.length-3]) == 1 && (parseInt(value[value.length-2]) >= 1 || parseInt(value[value.length-1]) >= 1 )){
+    return 'Cento';
+  } 
+  return dictionary.hundreds[parseInt(value[value.length-3])];
+}
+
+function thousandsConverter(value){
+  if (parseInt(value[value.length-4]) == 1){
+    return 'Mil';
+  } else {
+  return dictionary.units[parseInt(value[value.length-4])] + ' Mil';
+  }
+}
+
+function tenthsOfthousandsConverter(value){
+  if(parseInt(value[0]) == 1){
+    return dictionary.outliers[parseInt(value[1])];
+  } else {
+    return dictionary.tens[parseInt(value[0])];
+  }
+}
+
+module.exports = {
+    unitsConverter,
+    tensConverter,
+    hundredsConverter,
+    thousandsConverter,
+    tenthsOfthousandsConverter
+  };
