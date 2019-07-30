f4d1cf05cdde2672bb16403088d7dd43c76c4b74
Vitor Ferreira Bem Silva <vitorfbs.ti@gmail.com>
2019-07-30 14:43:06 -0300

    PT-BR number dictionary

---
+++ api/dictionaries/pt-br/NumberDictionary.js
@@ -0,0 +1,57 @@
+const units = [
+  "Zero",
+  "Um", 
+  "Dois", 
+  "Três", 
+  "Quatro", 
+  "Cinco", 
+  "Seis", 
+  "Sete", 
+  "Oito", 
+  "Nove"
+];
+
+const tens = [
+  "Dez", 
+  "Vinte", 
+  "Trinta", 
+  "Quarenta", 
+  "Cinquenta", 
+  "Sessenta", 
+  "Setenta", 
+  "Oitenta", 
+  "Noventa"
+];
+
+const hundreds = [
+  "Cem", 
+  "Duzentos", 
+  "Trezentos", 
+  "Quatrocentos", 
+  "Quinhentos", 
+  "Seiscentos", 
+  "Setescentos", 
+  "Oitocentos", 
+  "Novecentos"
+];
+
+const outliers = [
+  "Dez", 
+  "Onze", 
+  "Doze", 
+  "Treze", 
+  "Catorze", 
+  "Quinze", 
+  "Dezeseis", 
+  "Dezesete", 
+  "Dezoito", 
+  "Dezenove"
+];
+
+module.exports = {
+    units,
+    tens,
+    hundreds,
+    outliers,
+  };
+  
