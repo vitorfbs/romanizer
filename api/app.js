38f1114457b6990deb9a53bcc10ed8035c43cd7f
Vitor Ferreira Bem Silva <vitorfbs.ti@gmail.com>
2019-07-30 16:39:42 -0300

    Cleanup of unused dependancies in app.js

--- api/app.js
+++ api/app.js
@@ -1,21 +1,8 @@
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const handler = require('./errors/handler');
 
 // Routes
 const indexRouter = require('./routes/router');
 const app = express();
-
-// All midwares
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(handler.errorHandlerApi);
-app.use(cors());
 
 // Set all routes
 app.use('/', indexRouter);
