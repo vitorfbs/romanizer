1b8276664ea242a2e3cb9e8fd711a2c0ebdff0d2
Vitor Ferreira Bem Silva <vitorfbs.ti@gmail.com>
2019-07-30 22:07:07 -0300

    Status code response variable fix

--- api/handler/handler.js
+++ api/handler/handler.js
@@ -1,5 +1,6 @@
 const HTTPStatus = require('http-status');
 
+//successful request function, for returning the processed number
 function onRequestSuccess(response, data) {
   return response.status(HTTPStatus.OK).json({
     status: 200,
@@ -7,9 +8,10 @@ function onRequestSuccess(response, data) {
   });
 }
 
+//failure of request function, returning the error message
 function onRequestError(response, message) {
   return response.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
-    status_code: 500,
+    status: 500,
     message: [message],
   });
 }
